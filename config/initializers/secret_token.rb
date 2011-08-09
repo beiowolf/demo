658e8cdf9c848d70e2889cf66236f9bbb47fd13b
01Media <01media@01Media-iMac.local>
2011-08-09 12:10:29 +1000

    Initial Commit v0.001

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo::Application.config.secret_token = 'd6709522a0693a8ac906f9c050032c64ac5aa5b796add8b7d0492052735dacc140ed9127bb45fae2b85d7073a49ef7c4f4518161f52d55da855a794bb06de80c'
